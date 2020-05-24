e2ac0e44598222ef77ee0a482ac9010220d983f8
Glitch (hello-webpage) <none>
2020-05-24 04:06:16 +0000

    🎷📰 Checkpoint ./style.css:15663604/2496 ./index.html:15663604/64 ./script.js:15663604/825

--- script.js
+++ script.js
@@ -2,4 +2,28 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+
+let fortunes=[
+  
+  "Yes, but you will suffer greatly because of it.",
+  "It's murky, but what is clear is that they're all gonna laugh at you!",
+  "Not a chance!",
+  "Why do you even ask? You already know the answer",
+  "Yes. But upgrade your account to get the real answer!",
+  "Well, it depends on many factors, none of which you control.",
+  "If I tell you the answer, I will have to kill you.",
+  "42",
+  "Most likely, yes. But then again, you will probably screw it up."
+  
+  
+]
+
+
+
+
+
+function showFortune(){
+
+  
+
+}
